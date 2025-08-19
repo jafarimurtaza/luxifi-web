26769ad2109c0608fa65a56a78325bed47ff415d
Murtaza Jafari <65913750+jafarimurtaza@users.noreply.github.com>
2025-08-19 19:03:37 +0300

    Fixed tech path name

--- src/app/Tech/page.jsx
+++ src/app/Tech/page.jsx
@@ -2,7 +2,8 @@ import TechSection from "../components/Tech/TechSection";
 import FAQSection from "../components/Tech/FAQ/FAQSection";
 import HeroSection from "../components/Tech/HeroSection";
 import IntroImage from "../components/Layout/Images/IntroImage";
-export default function Tech() {
+
+export default function TechPage() {
   return (
     <>
       <IntroImage src={"/images/tech/intro-house-image.jpg"} alt="Hero Image" />
